6d6d5c3686b22bbd2cd315fcf1c4913063414176
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-08-20 11:53:18 +0530

    main updates

--- main.py
+++ main.py
@@ -4,15 +4,5 @@ from src.UberFares.logger import logging
 
 import os,sys
 
-
-def s():
-    try:
-
-        x=1/0
-        return x
-    except Exception as e:
-        raise CustomException(e, sys)
-
-
 if __name__ == '__main__':
     logging.info("everything is fine")
