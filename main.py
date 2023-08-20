74f266e509cc6a1eb0eb8f889a05bc22ef82a9f4
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-08-20 11:01:17 +0530

    add custom expection

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from src.UberFares.configuration.mongo_conn import MongodbClient
+from src.UberFares.exception import CustomException
+import os,sys
+
+
+def s():
+    try:
+
+        x=1/0
+        return x
+    except Exception as e:
+        raise CustomException(e, sys)
 
 
 if __name__ == '__main__':
-
-    mongo = MongodbClient()
-    print(mongo.database)
+    s()
+    # mongo = MongodbClient()
+    # print(mongo.database)
---
+++ src/UberFares/exception.py
@@ -0,0 +1,34 @@
+import sys
+
+
+# Creating function for error message detail
+def error_message_detail(error, error_detail: sys):
+    '''
+    This function will return the error message with the file name and line number
+    '''
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_no, str(error))
+    return error_message
+
+
+class CustomException(Exception):
+    '''
+    This class is used to raise custom exception
+    '''
+
+    def __init__(self, error_message, error_detail: sys):
+        '''
+        This function will initialize the error message
+        '''
+        super().__init__(error_message)
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
+
+    def __str__(self):
+        '''
+        This function will return the error message
+        '''
+        return self.error_message
