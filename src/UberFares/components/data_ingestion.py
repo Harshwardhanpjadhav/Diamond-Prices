be7ec42b8bd3592376b852fdf5db6183884ec984
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-08-23 21:24:37 +0530

    create ecport datafram

--- src/UberFares/components/data_ingestion.py
+++ src/UberFares/components/data_ingestion.py
@@ -3,6 +3,7 @@ from src.UberFares.exception import CustomException
 from src.UberFares.logger import logging
 from src.UberFares.entity.config import DataIngestionConfig
 from src.UberFares.entity.artifact import DataIngestionArtifact
+from src.UberFares.data_ascess.uber_data import UberData
 from pandas import DataFrame 
 
 
@@ -16,8 +17,13 @@ class DataIngestion:
 
     def export_data_to_feature_store(slef)->DataFrame:
         try:
-            pass
-
+            uber_data = UberData()
+            dataframe = uber_data.export_collection_as_dataframe(collection_name=slef.data_ingestion_config.collection_name)
+            feature_store_file_path = slef.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
 
         except Exception as e:
             raise CustomException(e,sys)
