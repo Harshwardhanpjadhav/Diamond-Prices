7e007810315cbad9c173930612e37e5a5fe2f88d
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-08-23 13:21:42 +0530

    Data_Ingestion_Config

--- src/UberFares/entity/config.py
+++ src/UberFares/entity/config.py
@@ -1,16 +1,26 @@
 from dataclasses import dataclass
 from datetime import datetime
 import os
-from src.UberFares.constants.trainingpipeline import PIPELINE_NAME,ARTIFACT_DIR
+from src.UberFares.constants import trainingpipeline as tp
 
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
        timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
-       self.pipeline_name:str = PIPELINE_NAME
-       self.artifact_dir:str = os.path.join(ARTIFACT_DIR,timestamp)
+       self.pipeline_name:str = tp.PIPELINE_NAME
+       self.artifact_dir:str = os.path.join(tp.ARTIFACT_DIR,timestamp)
        self.timestamp:str = timestamp
 
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,)
+        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir,tp.DATA_INGESTION_DIR_NAME)
+
+        self.feature_store_file_path = os.path.join(self.data_ingestion_dir,tp.DATA_INGESTION_FEATURE_STORE_DIR,tp.FILE_NAME)
+
+        self.training_file_path = os.path.join(self.data_ingestion_dir,tp.DATA_INGESTION_INGESTED_DIR,tp.TRAIN_FILE_NAME)
+
+        self.testing_file_path = os.path.join(self.data_ingestion_dir,tp.DATA_INGESTION_INGESTED_DIR,tp.TEST_FILE_NAME)
+
+        self.train_test_split_ratio:float = tp.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
+
+        self.collection_name:str = tp.DATA_INGESTION_COLLECTION_NAME
