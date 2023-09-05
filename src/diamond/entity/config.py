f381a0148fe30116845f436493067e1b72688acf
Harshwardhan <harshwardhanpj2001@gmail.com>
2023-09-05 09:57:55 +0530

    add data transformation config

--- src/diamond/entity/config.py
+++ src/diamond/entity/config.py
@@ -56,4 +56,9 @@ class DataValidationConfig:
 
 
 
-class DataTransformationConfig:...
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:tp):
+        self.data_transformation_dir:str = os.path.join(training_pipeline_config.artifact_dir,tp.DATA_TRANSFORMATION_DIR_NAME)
+        self.data_transformation_train_file_path:str = os.path.join(self.data_transformation_dir,tp.TRAIN_FILE_NAME)
+        self.data_transformation_test_file_path:str = os.path.join(self.data_transformation_dir,tp.TEST_FILE_NAME)
+        self.data_transformation_object_file_path:str = os.path.join(self.data_transformation_dir,tp.DATA_TRASNFORMATION_TRANSFORMED_DATA_OBJECT_DIR,tp.PREPROCESSING_PIPELINE_OBJECT)
